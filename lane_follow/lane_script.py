54d655cf79a94bb7309d0a0b7337dcb7c5eb2ab3
Andrew N <anguy424@ucsc.edu>
2024-02-15 14:51:26 -0800

    Adjusted code to generate environment from camera POV, model code is commented out at the moment

--- lane_follow/lane_script.py
+++ lane_follow/lane_script.py
@@ -1,5 +1,7 @@
 # This file needs work
 import carla
+import math
+import time
 import random
 import gymnasium as gym
 from stable_baselines3 import PPO
@@ -13,7 +15,6 @@ client = carla.Client('localhost', 2000)
 world = client.get_world()
 
 blueprint_library = world.get_blueprint_library()
-
 
 bp = random.choice(blueprint_library.filter('vehicle'))
 
@@ -31,6 +32,11 @@ transform = random.choice(world.get_map().get_spawn_points())
 # So let's tell the world to spawn the vehicle.
 ego_vehicle = world.spawn_actor(bp, transform)
 
+# Create a transform for the spectator
+spectator = world.get_spectator()
+spec_trans = carla.Transform(ego_vehicle.get_transform().transform(carla.Location(x = -4, z = 2.5)), ego_vehicle.get_transform().rotation)
+spectator.set_transform(spec_trans)
+
 # Create a transform to place the camera on top of the vehicle
 camera_init_trans = carla.Transform(carla.Location(z=1.5))
 
@@ -39,23 +45,26 @@ camera_bp = world.get_blueprint_library().find('sensor.camera.rgb')
 
 # We spawn the camera and attach it to our ego vehicle
 camera = world.spawn_actor(camera_bp, camera_init_trans, attach_to=ego_vehicle)
-# Model Control
 
-device = "cpu"
-model = PPO.load("../ppo-racecar.zip")
-cc = carla.ColorConverter.Raw
-control = carla.VehicleControl()
-while(True):
-    # Capture frame-by-frame
-    camera.listen(lambda image: image.save_to_disk('out/frame.png', cc)) # This does not seem to be generating the image file at the moment, work on this
+# Set spectator transform, allow time for spawning
+time.sleep(0.2)
+spectator.set_transform(camera.get_transform())
+camera.destroy()
+# # Model Control
 
-    frame = cv2.imread('_out/frame.png') 
-    # Our operations on the frame come here
-    img = cv2.resize(frame, (128,128))
-    img = np.transpose(img, (2, 0, 1))
-    action, _ = model.predict(img)
-    control.throttle = action[0]
-    control.steer = action[1]
-    ego_vehicle.apply_control(control)
-    
+# device = "cpu"
+# model = PPO.load("../ppo-racecar.zip")
+# cc = carla.ColorConverter.Raw
+# control = carla.VehicleControl()
+# while(True):
+#     # Capture frame-by-frame
+#     camera.listen(lambda image: image.save_to_disk('out/frame.png', cc)) # This does not seem to be generating the image file at the moment, work on this
 
+#     frame = cv2.imread('_out/frame.png') 
+#     # Our operations on the frame come here
+#     img = cv2.resize(frame, (128,128))
+#     img = np.transpose(img, (2, 0, 1))
+#     action, _ = model.predict(img)
+#     control.throttle = action[0]
+#     control.steer = action[1]
+#     ego_vehicle.apply_control(control)
