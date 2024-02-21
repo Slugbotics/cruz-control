2d184cdf83158fc2abd90424687f4bf61ce1edac
brendanRose1 <57075608+brendanRose1@users.noreply.github.com>
2024-02-20 18:34:49 -0800

    Added Pi control of servos in bluetooth_test.py
    added servo library, clean up code, add comments

--- Pi/bluetooth_test.py
+++ Pi/bluetooth_test.py
@@ -1,64 +1,64 @@
-from smbus import SMBus
+from gpiozero import Servo
+from enum import Enum
 import pygame
+
+# initialize pygame for services like joystick detection
 pygame.init()
 
-import os
-os.environ['SDL_JOYSTICK_HIDAPI_XBOX'] = '1'
-os.environ['SDL_JOYSTICK_HIDAPI_XBOX_ONE'] = '1'
+# initialize controller under pygame
 Controller = pygame.joystick.Joystick(0)
 Controller.init()
 print ("Detected joystick " + Controller.get_name())
 
-
+# values to store joystick state
 triggers = [-1.0,-1.0] #left joystick, right joystick [-1 default, 1 pressed]
 joystick1 = [0,0] #x axis [-1 left, 1 right] y axis [-1 up, 1 down]
 joystick2 = [0,0] #x axis [-1 left, 1 right] y axis [-1 up, 1 down]
 dpad = [0,0] #x axis [-1 left, 1 right] y axis [-1 down, 1 up]
-buttons = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0] # a b x y lb rb share option lhat rhat
+buttons = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] # a b x y lb rb share option lhat rhat
 
-maxSteer = 50
-maxThrottle = 20
+maxSteer = 50 # max angle (degree)
+maxThrottle = 20 # max throttle (percent)
 
+# values to write to servos
+angle = 0 
+throttle = 0
+
+# initialize servo objects on gpio 11, 12
+SteeringServo = Servo(11) 
+DrivingServo = Servo(12)
+
+# function to round num to range, returns 2 decimal point value
 def num_to_range(num, inMin, inMax, outMin, outMax):
-  return int(outMin + (float(num - inMin) / float(inMax - inMin) * (outMax- outMin)))
+  return round(outMin + (float(num - inMin) / float(inMax - inMin) * (outMax- outMin)), 2)
 
-addr = 0x8 # Pico communication bus on address 4
+# enum mappings for xbox controller readability
+# triggers and joysticks are registered under the axis event, use enum for clarity
+class XB_AXIS_MAP(Enum):
+     LTRIGGER=4
+     RTRIGGER=5
+     LJOYX=0
+     LJOYY=1
+     RJOYX=2
+     RJOYY=3
 
-def connect(port):
-	global bus
-	bus = SMBus(port)
-
-def string2Bytes(val):
-	retVal = []
-	for c in val:
-		retVal.append(ord(c))
-	return retVal
-
-import datetime
-def send(data):
-    # print(datetime.datetime.now())
-    bus.write_i2c_block_data(addr, 0x00, data) 
-    return -1
-
-def recv():
-     pass
-
-connect(1)
-
+# main program loop
 while True:
+    SteeringServo.value = angle
+    DrivingServo.value = throttle
     for event in pygame.event.get():
         if event.type == pygame.JOYAXISMOTION:
-            if event.axis == 4:
+            if event.axis == XB_AXIS_MAP.LTRIGGER:
                 triggers[0]=event.value + 1
-            if event.axis == 5:
+            if event.axis == XB_AXIS_MAP.RTRIGGER:
                 triggers[1]=event.value + 1
-            if event.axis == 0:
+            if event.axis == XB_AXIS_MAP.LJOYX:
                 joystick1[0]=event.value
-            if event.axis == 1:
+            if event.axis == XB_AXIS_MAP.LJOYY:
                 joystick1[1]=event.value
-            if event.axis == 2:
+            if event.axis == XB_AXIS_MAP.RJOYX:
                 joystick2[0]=event.value
-            if event.axis == 3:
+            if event.axis == XB_AXIS_MAP.RJOYY:
                 joystick2[1]=event.value
         if event.type == pygame.JOYHATMOTION: #d-pad
             dpad=[event.value[0],event.value[1]]
@@ -70,24 +70,11 @@ while True:
         #on pi, triggers[0] and [1] is flipped, windows order, triggers[1]-triggers[0]
         combinedTriggerValue = triggers[0]/2 - triggers[1]/2
 
-        if combinedTriggerValue > 1:
-             combinedTriggerValue = 1
-        if combinedTriggerValue < -1:
-             combinedTriggerValue = -1
-
-        throttle = num_to_range(combinedTriggerValue, -1, 1, -maxThrottle, maxThrottle)
-        angle = num_to_range(joystick1[0], -1, 1, -maxSteer, maxSteer)
+        # servo library breaks with values over expected range of -1, 1
+        # ranges of angle and throttle are scaled to max steering angle 
+        throttle = num_to_range(combinedTriggerValue, -1, 1, -maxThrottle/100, maxThrottle/100)
+        angle = num_to_range(joystick1[0], -1, 1, -maxSteer/90, maxSteer/90)
         
-
-        sendString =  str(angle) + "," + str(throttle)
-
-        print(sendString)
-	
-        angleSign = 0
-        throttleSign = 0
-        if angle > 0:
-             angleSign = 1
-        if throttle > 0:
-             throttleSign = 1
-        # send([angleSign, abs(angle), throttleSign, abs(throttle)])
-
+        # print values
+        print(str(throttle)+" "+str(angle))
+        
